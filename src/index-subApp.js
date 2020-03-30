9ca0f31e5372777cfc311d992b1482b847d32202
David M. Weigl <weigl@mdw.ac.at>
2020-03-30 20:08:56 +0200

    remove unused file

--- src/index-subApp.js
+++
@@ -1,9 +0,0 @@
-import React  from 'react';
-import ReactDOM from 'react-dom';
-
-import SubApp from './containers/subApp';
-
-ReactDOM.render(
-  <SubApp />
-		, document.querySelector('.container')
-);
