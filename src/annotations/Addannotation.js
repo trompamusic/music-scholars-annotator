fd89ebc74b62c92c47a0aa0511436f47afe0fcea
federico zubani <fzuba001@gold.ac.uk>
2020-08-20 11:24:10 +0100

    tidyup

--- src/annotations/Addannotation.js
+++ src/annotations/Addannotation.js
@@ -9,59 +9,50 @@ export class Addannotation extends React.Component {
   };
 
   onChange = (e) => this.setState({ value: e.target.value });
-  onSubmit = (e) => {
-    e.preventDefault();
-    this.setState(
-      {
-        target: [...this.state.target],
-      },
-      () => {
-        this.props.addannotation(
-          this.props.selection.map((elem) => {
-            return { id: this.props.uri + "#" + elem.getAttribute("id") };
-          }),
-          this.state.value
-        );
-        this.reset();
-        //this.props.buttonEnabler();
-      }
-    );
-  };
+  // onSubmit = (e) => {
+  //   e.preventDefault();
+  //   this.setState(
+  //     {
+  //       target: [...this.state.target],
+  //     },
+  //     () => {
+  //       this.props.addannotation(
+  //         this.props.selection.map((elem) => {
+  //           return { id: this.props.uri + "#" + elem.getAttribute("id") };
+  //         }),
+  //         this.state.value
+  //       );
+  //       this.reset();
+  //       //this.props.buttonEnabler();
+  //     }
+  //   );
+  // };
   //clears input field
-  reset = () => {
-    this.setState({ value: "" });
-  };
+  // reset = () => {
+  //   this.setState({ value: "" });
+  // };
 
   render() {
     return (
-      <form id="myform" onSubmit={this.onSubmit} style={{ display: "flex" }}>
-        <input
-          type="text"
-          name="value"
-          value={this.state.value}
-          placeholder={this.props.placeholder}
-          onChange={this.onChange}
-        />
-        {/* <input
-          type="submit"
-          name="submit"
-          disabled={!this.state.value}
-          className="selectionButton"
-          value="confirm your annotation"
-        /> */}
-        <button className={this.state.buttonState} form="myform" type="submit">
+      <div>
+        <form id="myform" style={{ display: "inline-block" }}>
+          <input
+            type="text"
+            name="value"
+            value={this.state.value}
+            placeholder={this.props.placeholder}
+            onChange={this.onChange}
+          />
+        </form>
+        <div className="enabledSubmitButton">
           <SubmitButton
             buttonContent="Submit to your Solid POD"
             submitUri={this.props.submitUri}
             submitHandler={this.props.submitHandler}
             submitHandlerArgs={{ value: this.state.value }}
           />
-        </button>
-
-        {/* <button className={this.state.buttonState} form="myform" type="submit">
-         
-        </button> */}
-      </form>
+        </div>
+      </div>
     );
   }
 }
