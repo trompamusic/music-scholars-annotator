7711ec686ebdce4d177467a802bc7c1beb5ec011
fedezubo <fzuba001@gold.ac.uk>
2021-02-25 16:19:51 +0000

    Updates

---
+++ precache-manifest.46a7bf3f0d9deb968c6da13f1e6af16b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2920e7aabd46f0146870c4aa3b3b57b",
+    "url": "/music-scholars-annotator/index.html"
+  },
+  {
+    "revision": "50b20d6cf692b15621d7",
+    "url": "/music-scholars-annotator/static/js/2.e83a854c.chunk.js"
+  },
+  {
+    "revision": "6d4b83bcd2ff821074937756016ad73b",
+    "url": "/music-scholars-annotator/static/js/2.e83a854c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a960d6b248343201934f",
+    "url": "/music-scholars-annotator/static/js/main.0664490e.chunk.js"
+  },
+  {
+    "revision": "bca5414a5da2f43f11d1",
+    "url": "/music-scholars-annotator/static/js/runtime-main.942e566b.js"
+  },
+  {
+    "revision": "f79fe6ae99a50939f12f755851398cf9",
+    "url": "/music-scholars-annotator/static/media/play-solid.f79fe6ae.svg"
+  }
+]);
