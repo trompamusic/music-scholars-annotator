99458a81aa9c4ca656ec9a6b7229a5fce024f2ca
fedezubo <fzuba001@gold.ac.uk>
2021-03-03 20:35:54 +0000

    styled delete button and added delte+info to replies

--- src/annotations/annotationItem.js
+++ src/annotations/annotationItem.js
@@ -73,7 +73,25 @@ class AnnotationItem extends React.Component {
     e.stopPropagation();
     const parent = e.target.closest(".rootAnno");
     const details = parent.querySelector(".hiddenDetails");
-    console.log(parent, " with this details ", details);
+    if (details && this.state.isVisible === false) {
+      this.setState({ isVisible: true });
+      details.classList.remove("hiddenDetails");
+      details.classList.add("showDetails");
+    } else {
+      if (this.state.isVisible === true) {
+        this.setState({ isVisible: false });
+        const visibleDetails = parent.querySelector(".showDetails");
+        visibleDetails.classList.remove("showDetails");
+        visibleDetails.classList.add("hiddenDetails");
+      }
+    }
+  };
+
+  showReplyDetails = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const parent = e.target.closest(".quoteContent");
+    const details = parent.querySelector(".hiddenDetails");
     if (details && this.state.isVisible === false) {
       this.setState({ isVisible: true });
       details.classList.remove("hiddenDetails");
@@ -353,9 +371,7 @@ class AnnotationItem extends React.Component {
     // TODO allow sharing with individual agents using setAgentResourceAccess
     if (!this.state.userMayModifyAccess) {
       modifyPermissionsElement = (
-        <span className="accessPermissions">
-          User may <b>not</b> modify access
-        </span>
+        <span className="accessPermissions">No access</span>
       );
     } else {
       if (permission !== "public") {
@@ -445,6 +461,23 @@ class AnnotationItem extends React.Component {
                 Created on: {date}, access permissions: {permission}.
               </span>
             </span>
+            <button className="deleteButton" onClick={this.deleteAnno}>
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="trash"
+                className="svg-inline--fa fa-trash fa-w-14"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="grey"
+                  d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
+                ></path>
+              </svg>
+            </button>
             <button
               className="infoButton"
               onMouseEnter={this.showDetails}
@@ -468,20 +501,19 @@ class AnnotationItem extends React.Component {
             </button>
             <p></p>
             permissions: {modifyPermissionsElement}
-            <button onClick={this.deleteAnno}>del</button>
-            <button
-              className="replyButton"
-              name="replyButton"
-              onClick={this.onClick}
-            >
-              Reply
-            </button>
             <button
               className="showRepliesButton"
               name="showRepliesButton"
               onClick={this.onShowReplyClick}
             >
               {this.state.showReplyButtonContent}
+            </button>
+            <button
+              className="replyButton"
+              name="replyButton"
+              onClick={this.onClick}
+            >
+              Reply
             </button>
           </div>
         );
@@ -512,6 +544,23 @@ class AnnotationItem extends React.Component {
                   Created on: {date}, access permissions: {permission}.
                 </span>
               </span>
+              <button className="deleteButton" onClick={this.deleteAnno}>
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="trash"
+                  className="svg-inline--fa fa-trash fa-w-14"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    fill="grey"
+                    d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
+                  ></path>
+                </svg>
+              </button>
               <button
                 className="infoButton"
                 onMouseEnter={this.showDetails}
@@ -535,20 +584,19 @@ class AnnotationItem extends React.Component {
               </button>
               <p></p>
               Set permissions: {modifyPermissionsElement}
-              <button onClick={this.deleteAnno}>del</button>
-              <button
-                className="replyButton"
-                name="replyButton"
-                onClick={this.onClick}
-              >
-                Reply
-              </button>
               <button
                 className="showRepliesButton"
                 name="showRepliesButton"
                 onClick={this.onShowReplyClick}
               >
                 {this.state.showReplyButtonContent}
+              </button>
+              <button
+                className="replyButton"
+                name="replyButton"
+                onClick={this.onClick}
+              >
+                Reply
               </button>
             </div>
           );
@@ -579,6 +627,23 @@ class AnnotationItem extends React.Component {
                   Created on: {date}, access permissions: {permission}.
                 </span>
               </span>
+              <button className="deleteButton" onClick={this.deleteAnno}>
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="trash"
+                  className="svg-inline--fa fa-trash fa-w-14"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    fill="grey"
+                    d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
+                  ></path>
+                </svg>
+              </button>
               <button
                 className="infoButton"
                 onMouseEnter={this.showDetails}
@@ -602,20 +667,19 @@ class AnnotationItem extends React.Component {
               </button>
               <p></p>
               Set permissions: {modifyPermissionsElement}
-              <button onClick={this.deleteAnno}>del</button>
-              <button
-                className="replyButton"
-                name="replyButton"
-                onClick={this.onClick}
-              >
-                Reply
-              </button>
               <button
                 className="showRepliesButton"
                 name="showRepliesButton"
                 onClick={this.onShowReplyClick}
               >
                 {this.state.showReplyButtonContent}
+              </button>
+              <button
+                className="replyButton"
+                name="replyButton"
+                onClick={this.onClick}
+              >
+                Reply
               </button>
             </div>
           );
@@ -652,6 +716,23 @@ class AnnotationItem extends React.Component {
                 Created on: {date}, access permissions: {permission}.
               </span>
             </span>
+            <button className="deleteButton" onClick={this.deleteAnno}>
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="trash"
+                className="svg-inline--fa fa-trash fa-w-14"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="grey"
+                  d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
+                ></path>
+              </svg>
+            </button>
             <button
               className="infoButton"
               onMouseEnter={this.showDetails}
@@ -675,20 +756,19 @@ class AnnotationItem extends React.Component {
             </button>
             <p></p>
             Set permissions: {modifyPermissionsElement}
-            <button onClick={this.deleteAnno}>del</button>
-            <button
-              className="replyButton"
-              name="replyButton"
-              onClick={this.onClick}
-            >
-              Reply
-            </button>
             <button
               className="showRepliesButton"
               name="showRepliesButton"
               onClick={this.onShowReplyClick}
             >
               {this.state.showReplyButtonContent}
+            </button>
+            <button
+              className="replyButton"
+              name="replyButton"
+              onClick={this.onClick}
+            >
+              Reply
             </button>
           </div>
         );
@@ -727,6 +807,23 @@ class AnnotationItem extends React.Component {
                 Created on: {date}, access permissions: {permission}.
               </span>
             </span>
+            <button className="deleteButton" onClick={this.deleteAnno}>
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="trash"
+                className="svg-inline--fa fa-trash fa-w-14"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="grey"
+                  d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
+                ></path>
+              </svg>
+            </button>
             <button
               className="infoButton"
               onMouseEnter={this.showDetails}
@@ -750,20 +847,19 @@ class AnnotationItem extends React.Component {
             </button>
             <p></p>
             Set permissions: {modifyPermissionsElement}
-            <button onClick={this.deleteAnno}>del</button>
-            <button
-              className="replyButton"
-              name="replyButton"
-              onClick={this.onClick}
-            >
-              Reply
-            </button>
             <button
               className="showRepliesButton"
               name="showRepliesButton"
               onClick={this.onShowReplyClick}
             >
               {this.state.showReplyButtonContent}
+            </button>
+            <button
+              className="replyButton"
+              name="replyButton"
+              onClick={this.onClick}
+            >
+              Reply
             </button>
           </div>
         );
@@ -776,7 +872,50 @@ class AnnotationItem extends React.Component {
           >
             <div className="quoteContent">
               <p>Reply: {bodyD}</p>
-              <span className="date">Created on: {date}</span>
+              <span className="hiddenDetails">
+                {" "}
+                <span className="date">
+                  Created on: {date}, access permissions: {permission}.
+                </span>
+              </span>
+              <button className="deleteButton" onClick={this.deleteAnno}>
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="trash"
+                  className="svg-inline--fa fa-trash fa-w-14"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    fill="grey"
+                    d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
+                  ></path>
+                </svg>
+              </button>
+              <button
+                className="infoButton"
+                onMouseEnter={this.showReplyDetails}
+                onMouseLeave={this.showReplyDetails}
+              >
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="far"
+                  data-icon="info-circle"
+                  className="svg-inline--fa fa-info-circle fa-w-16"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="grey"
+                    d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm0-338c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
+                  ></path>
+                </svg>
+              </button>
             </div>
           </div>
         );
