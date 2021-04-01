ee8dd320d936645134552a67bbf8311a9b259939
fedezubo <fzuba001@gold.ac.uk>
2021-04-01 12:43:16 +0100

    Updates

---
+++ precache-manifest.16a478e51d4d8b5728696771c3ee37e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b05aa218ce755a12fb2490088d251b",
+    "url": "/music-scholars-annotator/index.html"
+  },
+  {
+    "revision": "87b233d80e4e59fcbac0",
+    "url": "/music-scholars-annotator/static/js/2.b03b6e60.chunk.js"
+  },
+  {
+    "revision": "6d4b83bcd2ff821074937756016ad73b",
+    "url": "/music-scholars-annotator/static/js/2.b03b6e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bf0a5750d38374c2da",
+    "url": "/music-scholars-annotator/static/js/main.3ce6f8be.chunk.js"
+  },
+  {
+    "revision": "bca5414a5da2f43f11d1",
+    "url": "/music-scholars-annotator/static/js/runtime-main.942e566b.js"
+  },
+  {
+    "revision": "f79fe6ae99a50939f12f755851398cf9",
+    "url": "/music-scholars-annotator/static/media/play-solid.f79fe6ae.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6831eb91e0997b68c81f9fb368af53e1.js"
+  "/music-scholars-annotator/precache-manifest.16a478e51d4d8b5728696771c3ee37e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music-scholars-annotator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
