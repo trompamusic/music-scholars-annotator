0eff7a53a9b4aaa880abbecb7ce558d4a79face8
fedezubo <fzuba001@gold.ac.uk>
2021-04-27 12:45:42 +0100

    styled search box

--- src/containers/FileSelector.jsx
+++ src/containers/FileSelector.jsx
@@ -1,171 +1,180 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {useQuery} from "@apollo/client";
-import {gql} from "@apollo/client/core";
+import { useQuery } from "@apollo/client";
+import { gql } from "@apollo/client/core";
+import searchIcon from "../graphics/search-solid.svg";
 
 const EARLY_MUSIC_WORKS = gql`
-query earlyMusicWorks($query: String!) {
-  ItemList(identifier:"e91489d7-a776-40dd-8abf-0c934922bd99") {
-    identifier
-    name
-    itemListElement(filter:{name_regexp:$query}) {
-      __typename
-      ... on MusicComposition {
-        identifier
-        contributor
-        name
-        source
-        composer {
+  query earlyMusicWorks($query: String!) {
+    ItemList(identifier: "e91489d7-a776-40dd-8abf-0c934922bd99") {
+      identifier
+      name
+      itemListElement(filter: { name_regexp: $query }) {
+        __typename
+        ... on MusicComposition {
+          identifier
+          contributor
           name
-        }
-        workExample(filter: {encodingFormat:"application/mei+xml"}) {
-          ... on MediaObject {
-            identifier
-            source
-            contentUrl
-            encodingFormat
+          source
+          composer {
+            name
+          }
+          workExample(filter: { encodingFormat: "application/mei+xml" }) {
+            ... on MediaObject {
+              identifier
+              source
+              contentUrl
+              encodingFormat
+            }
           }
         }
       }
     }
   }
-}
 `;
 
 function SearchResults(props) {
+  const query = `(?i).*${props.query}.*`;
+  const { loading, error, data } = useQuery(EARLY_MUSIC_WORKS, {
+    variables: { query: query },
+  });
 
-    const query = `(?i).*${props.query}.*`
-    const { loading, error, data } = useQuery(EARLY_MUSIC_WORKS, {variables: {query: query}});
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+  console.debug(data);
 
-    console.debug(data)
-
-    const works = data.ItemList;
-    if (!works || !works.length) {
-        return <div/>
-    }
-    console.debug(works)
-    const compositions = works[0].itemListElement;
-    if (!compositions || !compositions.length) {
-        return <div/>
-    }
-    console.debug(compositions)
-    return (
-        <div>
-            <p>Results:</p>
-            <ul>
-                {compositions.map(function(item) {
-                    const id = item.identifier
-                    const title = item.name;
-                    const contributor = item.contributor;
-                    const composer = item.composer[0].name;
-                    const workExample = item.workExample;
-                    if (workExample && workExample.length) {
-                        return <li key={id}>
-                            <a href={workExample[0].source} onClick={props.onSelect}>{composer} - {title} (from {contributor})</a>
-                        </li>
-                    } else {
-                        return ""
-                    }
-                })}
-            </ul>
-        </div>
-    )
+  const works = data.ItemList;
+  if (!works || !works.length) {
+    return <div />;
+  }
+  console.debug(works);
+  const compositions = works[0].itemListElement;
+  if (!compositions || !compositions.length) {
+    return <div />;
+  }
+  console.debug(compositions);
+  return (
+    <div>
+      <p>Results:</p>
+      <ul>
+        {compositions.map(function (item) {
+          const id = item.identifier;
+          const title = item.name;
+          const contributor = item.contributor;
+          const composer = item.composer[0].name;
+          const workExample = item.workExample;
+          if (workExample && workExample.length) {
+            return (
+              <li key={id}>
+                <a href={workExample[0].source} onClick={props.onSelect}>
+                  {composer} - {title} (from {contributor})
+                </a>
+              </li>
+            );
+          } else {
+            return "";
+          }
+        })}
+      </ul>
+    </div>
+  );
 }
 
 SearchResults.props = {
-    query: PropTypes.string,
-    onSelect: PropTypes.func
-}
+  query: PropTypes.string,
+  onSelect: PropTypes.func,
+};
 
 export default class FileSelector extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            // default file to load
-            defaultValue: this.props.defaultUrl,
-            // react controlled component for url field
-            fieldValue: '',
-            // react controlled component for search field
-            searchValue: '',
-            showResults: false,
-            searchResults: {}
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      // default file to load
+      defaultValue: this.props.defaultUrl,
+      // react controlled component for url field
+      fieldValue: "",
+      // react controlled component for search field
+      searchValue: "",
+      showResults: false,
+      searchResults: {},
+    };
+  }
 
-    handleChange = event => {
-        this.setState({fieldValue: event.target.value});
-    }
+  handleChange = (event) => {
+    this.setState({ fieldValue: event.target.value });
+  };
 
-    handleSearchChange = event => {
-        this.setState({searchValue: event.target.value});
-    }
+  handleSearchChange = (event) => {
+    this.setState({ searchValue: event.target.value });
+  };
 
-    handleSubmit = event => {
-        event.preventDefault();
-        this.props.onSelect(this.state.fieldValue || this.state.defaultValue);
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSelect(this.state.fieldValue || this.state.defaultValue);
+  };
 
-    handleSearch = event => {
-        event.preventDefault();
-        this.setState({showResults: true})
-    }
+  handleSearch = (event) => {
+    event.preventDefault();
+    this.setState({ showResults: true });
+  };
 
-    handleSearchResultSelection = event => {
-        event.preventDefault();
-        this.props.onSelect(event.target.attributes.href.value);
-    }
+  handleSearchResultSelection = (event) => {
+    event.preventDefault();
+    this.props.onSelect(event.target.attributes.href.value);
+  };
 
-    render() {
-        return (
-            <div>
-                <p>Select your MEI file:</p>
-                <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    onChange={this.handleChange}
-                    value={this.state.fieldValue}
-                    placeholder={this.props.placeholder}
-                    className="sizedTextBox"
-                />
+  render() {
+    return (
+      <div>
+        <p>Select your MEI file:</p>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.fieldValue}
+            placeholder="https://..."
+            className="sizedTextBox"
+          />
 
-                <input
-                    title="click to render the linked MEI file"
-                    className="MEIButton"
-                    type="submit"
-                    value="render"
-                />
-                </form>
-                <p>Or search for an item from the TROMPA database</p>
-                <form onSubmit={this.handleSearch}>
-                    <input
-                        type="text"
-                        onChange={this.handleSearchChange}
-                        value={this.state.searchValue}
-                        placeholder="search for a work name"
-                        className="sizedTextBox"
-                    />
-
-                    <input
-                        title="search for a work name"
-                        className="MEIButton"
-                        type="submit"
-                        value="search"
-                    />
-                </form>
-                {this.state.showResults &&
-                    <SearchResults query={this.state.searchValue} onSelect={this.handleSearchResultSelection}/>
-                }
-
-            </div>
-        );
-    }
+          <input
+            title="click to render the linked MEI file"
+            className="MEIButton"
+            type="submit"
+            value="render"
+          />
+        </form>
+        <p>Or search for an item from the TROMPA database</p>
+        <form onSubmit={this.handleSearch} className="hide-submit">
+          <input
+            type="text"
+            onChange={this.handleSearchChange}
+            value={this.state.searchValue}
+            placeholder="search for a work name"
+            className="sizedTextBox"
+          />
+          <label>
+            <input
+              title="search for a work name"
+              className="MEIButton"
+              type="submit"
+            />
+            <img src={searchIcon} alt="zoom in" className="searchIcon" />
+          </label>
+        </form>
+        {this.state.showResults && (
+          <SearchResults
+            query={this.state.searchValue}
+            onSelect={this.handleSearchResultSelection}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 FileSelector.propTypes = {
-    defaultUrl: PropTypes.string,
-    placeholder: PropTypes.string,
-    onSelect: PropTypes.func
-}
+  defaultUrl: PropTypes.string,
+  placeholder: PropTypes.string,
+  onSelect: PropTypes.func,
+};
--- src/containers/FtempoSearch.jsx
+++ src/containers/FtempoSearch.jsx
@@ -1,106 +1,137 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {parseMei} from "../search/SearchQuery";
+import { parseMei } from "../search/SearchQuery";
 import {
-    Accordion,
-    AccordionItem,
-    AccordionItemButton,
-    AccordionItemHeading,
-    AccordionItemPanel
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
 } from "react-accessible-accordion";
 
-
 export default class FtempoSearch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchReady: false,
-            selectedOption: null,
-            meiVoiceQueryStrings: {},
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchReady: false,
+      selectedOption: null,
+      meiVoiceQueryStrings: {},
+    };
+  }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (this.props.counter !== prevProps.counter) {
-            const mei = this.props.vrvToolkit.getMEI();
-            const meiDoc = new DOMParser().parseFromString(mei, "text/xml");
-            console.debug(meiDoc)
-            // TODO: not working?
-            if (meiDoc.documentElement.nodeName === "parsererror") {
-                return ""
-            }
-            this.setState({meiVoiceQueryStrings: parseMei(meiDoc)})
-        }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.counter !== prevProps.counter) {
+      const mei = this.props.vrvToolkit.getMEI();
+      const meiDoc = new DOMParser().parseFromString(mei, "text/xml");
+      console.debug(meiDoc);
+      // TODO: not working?
+      if (meiDoc.documentElement.nodeName === "parsererror") {
+        return "";
+      }
+      this.setState({ meiVoiceQueryStrings: parseMei(meiDoc) });
     }
+  }
 
-    buttonPressed = (event) => {
-        event.preventDefault();
-        this.setState({searchReady: true});
-        this.props.onButtonPress(event);
-    }
+  buttonPressed = (event) => {
+    event.preventDefault();
+    this.setState({ searchReady: true });
+    this.props.onButtonPress(event);
+  };
 
-    onValueChange = (event) => {
-        this.setState({
-            selectedOption: event.target.value
+  onValueChange = (event) => {
+    this.setState({
+      selectedOption: event.target.value,
+    });
+  };
+
+  doSearch = () => {
+    const selectedVoice = this.state.meiVoiceQueryStrings[
+      this.state.selectedOption
+    ];
+    if (selectedVoice && selectedVoice.notes) {
+      const query = { codestring: selectedVoice.notes };
+      fetch("https://porter.net.nz/ftempo/api/query", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(query),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({ searchResults: data });
         });
     }
+  };
 
-    doSearch = () => {
-        const selectedVoice = this.state.meiVoiceQueryStrings[this.state.selectedOption];
-        if (selectedVoice && selectedVoice.notes) {
-            const query = {codestring: selectedVoice.notes}
-            fetch('https://porter.net.nz/ftempo/api/query', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(query),
-            }).then(response => response.json()).then(data => {
-                this.setState({searchResults: data})
-            })
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <h3>Search using FTempo</h3>
-                {!this.state.searchReady && <button onClick={this.buttonPressed}>Show search options</button>}
-                {this.state.searchReady && this.state.meiVoiceQueryStrings &&
-                <><p>Search this score on FTempo, either a single voice or all voices</p>
-                <ul>
-                    {Object.keys(this.state.meiVoiceQueryStrings).map((e) => {
-                        return <li key={e}>
-                            <label><input type="radio" name="voice" value={e} id={"voice-" + e} checked={this.state.selectedOption === e} onChange={this.onValueChange}/>
-                                {this.state.meiVoiceQueryStrings[e].label}</label></li>
-                    })
-                    }
-                    {/*<li><label><input type="radio" name="voice" value="all" checked={this.state.selectedOption === "all"} onChange={this.onValueChange} />All voices</label></li>*/}
-                </ul>
-                    <button onClick={this.doSearch} disabled={!this.state.selectedOption}>Search</button>
-                </>
-                }
-                {this.state.searchResults &&
-                <Accordion allowZeroExpanded>
-                    {this.state.searchResults.map((i) => {
-                        return <AccordionItem key={i.id}>
-                            <AccordionItemHeading>
-                                <AccordionItemButton>{i.id}</AccordionItemButton>
-                            </AccordionItemHeading>
-                            <AccordionItemPanel>
-                                <img src={"https://porter.net.nz/ftempo/img/jpg/" + i.id + ".jpg"}/>
-                            </AccordionItemPanel>
-                        </AccordionItem>
-                })}
-                </Accordion>
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>Search using FTempo</h3>
+        {!this.state.searchReady && (
+          <button onClick={this.buttonPressed}>Show search options</button>
+        )}
+        {this.state.searchReady && this.state.meiVoiceQueryStrings && (
+          <>
+            <p>
+              Search this score on FTempo, either a single voice or all voices
+            </p>
+            <ul>
+              {Object.keys(this.state.meiVoiceQueryStrings).map((e) => {
+                return (
+                  <li key={e}>
+                    <label>
+                      <input
+                        type="radio"
+                        name="voice"
+                        value={e}
+                        id={"voice-" + e}
+                        checked={this.state.selectedOption === e}
+                        onChange={this.onValueChange}
+                      />
+                      {this.state.meiVoiceQueryStrings[e].label}
+                    </label>
+                  </li>
+                );
+              })}
+              {/*<li><label><input type="radio" name="voice" value="all" checked={this.state.selectedOption === "all"} onChange={this.onValueChange} />All voices</label></li>*/}
+            </ul>
+            <button
+              onClick={this.doSearch}
+              disabled={!this.state.selectedOption}
+            >
+              Search
+            </button>
+          </>
+        )}
+        {this.state.searchResults && (
+          <Accordion allowZeroExpanded>
+            {this.state.searchResults.map((i) => {
+              return (
+                <AccordionItem key={i.id}>
+                  <AccordionItemHeading>
+                    <AccordionItemButton>{i.id}</AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <img
+                      src={
+                        "https://porter.net.nz/ftempo/img/jpg/" + i.id + ".jpg"
+                      }
+                      alt=""
+                    />
+                  </AccordionItemPanel>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
+        )}
+      </div>
+    );
+  }
 }
 
 FtempoSearch.propTypes = {
-    onButtonPress: PropTypes.func,
-    vrvToolkit: PropTypes.any,
-    counter: PropTypes.number
-}
+  onButtonPress: PropTypes.func,
+  vrvToolkit: PropTypes.any,
+  counter: PropTypes.number,
+};
