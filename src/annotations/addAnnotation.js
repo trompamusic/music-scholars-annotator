28338c0c9c93e4595a336622ec9dfe5c938b9ea8
Alastair Porter <alastair@porter.net.nz>
2021-07-17 19:01:56 +0200

    Replace inline SVG with react components loaded from external file

--- src/annotations/addAnnotation.js
+++ src/annotations/addAnnotation.js
@@ -2,6 +2,9 @@
 /* also the input field changes based on the chosen annotation motivation*/
 import React from "react";
 import SubmitButton from "selectable-score/lib/submit-button.js";
+import {ReactComponent as ArrowAltToTop} from "../graphics/arrow-alt-to-top-regular.svg"
+import {ReactComponent as FileImport} from "../graphics/file-import-regular.svg"
+import {ReactComponent as RedoAlt} from "../graphics/redo-alt-solid.svg"
 
 export class AddAnnotation extends React.Component {
   state = {
@@ -10,6 +13,7 @@ export class AddAnnotation extends React.Component {
     target: [],
     buttonStatus: "disabledSubmitButton",
   };
+
   onChange = (e) => this.setState({ value: e.target.value });
 
   onTimeChange = (e) =>
@@ -90,24 +94,7 @@ export class AddAnnotation extends React.Component {
             className="enabledSubmitButton"
             title="click to post your annotation to your solid POD"
           >
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="far"
-              data-icon="arrow-alt-to-top"
-              className="svg-inline--fa fa-arrow-alt-to-top fa-w-12"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-              width="1em"
-              height="1em"
-              display="inline-flex"
-            >
-              <path
-                fill="white"
-                d="M48 336h51.6v96c0 26.5 21.5 48 48 48h88.6c26.5 0 48-21.5 48-48v-96h51.6c42.6 0 64.2-51.7 33.9-81.9l-143.9-144c-18.7-18.7-49.1-18.7-67.9 0l-144 144C-16 284.2 5.3 336 48 336zm144-192l144 144h-99.7v144h-88.6V288H48l144-144zM0 68V44c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12v24c0 6.6-5.4 12-12 12H12C5.4 80 0 74.6 0 68z"
-              ></path>
-            </svg>{" "}
+            <ArrowAltToTop style={{width: '1em', height: '1em'}} />{" "}
             <SubmitButton
               buttonContent={this.props.buttonContent}
               submitUri={this.props.submitUri}
@@ -122,24 +109,7 @@ export class AddAnnotation extends React.Component {
             className="disabledSubmitButton"
             title="click to post your annotation to your solid POD"
           >
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="far"
-              data-icon="file-import"
-              className="svg-inline--fa fa-file-import fa-w-16"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              width="1em"
-              height="1em"
-              display="inline-flex"
-            >
-              <path
-                fill="white"
-                d="M497.83 97.98L413.94 14.1c-9-9-21.2-14.1-33.89-14.1H175.99C149.5.1 128 21.6 128 48.09v215.98H12c-6.63 0-12 5.37-12 12v24c0 6.63 5.37 12 12 12h276v48.88c0 10.71 12.97 16.05 20.52 8.45l71.77-72.31c4.95-4.99 4.95-13.04 0-18.03l-71.77-72.31c-7.55-7.6-20.52-2.26-20.52 8.45v48.88H175.99V48.09h159.97v103.98c0 13.3 10.7 23.99 24 23.99H464v287.95H175.99V360.07H128v103.94c0 26.49 21.5 47.99 47.99 47.99h287.94c26.5 0 48.07-21.5 48.07-47.99V131.97c0-12.69-5.17-24.99-14.17-33.99zm-113.88 30.09V51.99l76.09 76.08h-76.09z"
-              ></path>
-            </svg>{" "}
+            <FileImport style={{width: '1em', height: '1em'}} />{" "}
             <SubmitButton
               buttonContent="Write something to begin..."
               submitUri={this.props.submitUri}
@@ -155,25 +125,7 @@ export class AddAnnotation extends React.Component {
           className="refreshButton"
           title="click to display the annotation contained in your solid POD"
         >
-          <svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="redo-alt"
-            className="svg-inline--fa fa-redo-alt fa-w-16"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            width="1em"
-            height="1em"
-            display="inline-flex"
-          >
-            <path
-              fill="white"
-              d="M256.455 8c66.269.119 126.437 26.233 170.859 68.685l35.715-35.715C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.75c-30.864-28.899-70.801-44.907-113.23-45.273-92.398-.798-170.283 73.977-169.484 169.442C88.764 348.009 162.184 424 256 424c41.127 0 79.997-14.678 110.629-41.556 4.743-4.161 11.906-3.908 16.368.553l39.662 39.662c4.872 4.872 4.631 12.815-.482 17.433C378.202 479.813 319.926 504 256 504 119.034 504 8.001 392.967 8 256.002 7.999 119.193 119.646 7.755 256.455 8z"
-            ></path>
-          </svg>
-
+          <RedoAlt style={{width: '1em', height: '1em'}} />
           <span>{""} Fetch Annotations</span>
         </button>
       </div>
@@ -182,34 +134,3 @@ export class AddAnnotation extends React.Component {
 }
 
 export default AddAnnotation;
-
-/* old */
-/* <button
-          style={{ padding: "5px", marginBottom: "5px" }}
-          onClick={() => this.setState({ visible: !visible })}
-        >
-          Toggle legend
-        </button>
-        {this.state.visible === true && (
-          <div>
-            <h4>Annotation legend</h4>
-
-            <span className="selectionLegend" />
-            <span>currently selected</span>
-
-            <span className="descriptionLegend" />
-            <span>descriptive annotation</span>
-
-            <span className="linkingLegend" />
-            <span>linking annotation</span>
-
-            <span className="cueMediaLegend" />
-            <span>cue media annotation</span>
-
-            <span className="replyLegend" />
-            <span>replying annotation</span>
-
-            <span className="focusLegend" />
-            <span>highlighted annotation</span>
-          </div>
-        )} */
--- src/annotations/annotationItem.js
+++ src/annotations/annotationItem.js
@@ -7,20 +7,20 @@ import {
   hasResourceAcl,
   hasFallbackAcl,
   hasAccessibleAcl,
-  // eslint-disable-next-line
-  createAcl,
   createAclFromFallbackAcl,
   getResourceAcl,
   setAgentResourceAccess,
   setPublicResourceAccess,
   saveAclFor,
-  // eslint-disable-next-line
-  getSolidDataset,
   getPublicAccess,
   getAgentAccessAll,
 } from "@inrupt/solid-client";
 
 import PlayLogo from "../graphics/play-solid.svg";
+import {ReactComponent as Trash} from "../graphics/trash-solid.svg";
+import {ReactComponent as InfoCircle} from "../graphics/info-circle-regular.svg";
+
+
 class AnnotationItem extends React.Component {
   constructor(props) {
     super(props);
@@ -493,23 +493,7 @@ class AnnotationItem extends React.Component {
         <div style={{ paddingTop: "1.5%" }}>
           {compareDate === creationDate && (
             <button className="deleteButton" onClick={this.showConfirm}>
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="trash"
-                className="svg-inline--fa fa-trash fa-w-14"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-                width="100%"
-                height="100%"
-              >
-                <path
-                  fill="grey"
-                  d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
-                ></path>
-              </svg>
+              <Trash />
             </button>
           )}
           <button
@@ -517,23 +501,7 @@ class AnnotationItem extends React.Component {
             onMouseEnter={this.showDetails}
             onMouseLeave={this.showDetails}
           >
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="far"
-              data-icon="info-circle"
-              className="svg-inline--fa fa-info-circle fa-w-16"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              width="100%"
-              height="100%"
-            >
-              <path
-                fill="grey"
-                d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm0-338c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
-              ></path>
-            </svg>
+            <InfoCircle/>
           </button>
           permissions: {modifyPermissionsElement}
         </div>
@@ -718,46 +686,14 @@ class AnnotationItem extends React.Component {
           >
             <div className="quoteContent">
               <button className="deleteButton" onClick={this.deleteAnno}>
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fas"
-                  data-icon="trash"
-                  className="svg-inline--fa fa-trash fa-w-14"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                  width="100%"
-                  height="100%"
-                >
-                  <path
-                    fill="grey"
-                    d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
-                  ></path>
-                </svg>
+                <Trash/>
               </button>
               <button
                 className="infoButton"
                 onMouseEnter={this.showReplyDetails}
                 onMouseLeave={this.showReplyDetails}
               >
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="far"
-                  data-icon="info-circle"
-                  className="svg-inline--fa fa-info-circle fa-w-16"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                  width="100%"
-                  height="100%"
-                >
-                  <path
-                    fill="grey"
-                    d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm0-338c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
-                  ></path>
-                </svg>
+                <InfoCircle/>
               </button>
               <p>Reply: {bodyD}</p>
               <span className="hiddenDetails">
