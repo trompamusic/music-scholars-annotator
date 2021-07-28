09661baa64c55da32e259b8eb4b58e4cebc26fbf
Alastair Porter <alastair@porter.net.nz>
2021-07-28 16:11:20 +0200

    Add config to allow router to work at a different base url

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+const router_basename = process.env.REACT_APP_ROUTER_BASENAME;
+
 ReactDOM.render(
   // Load BrowserRouter as soon as possible so that we can use reactrouter
   // hooks in App
-  <BrowserRouter><App/></BrowserRouter>,
+  <BrowserRouter basename={router_basename}>
+    <App />
+  </BrowserRouter>,
   document.querySelector(".container")
 );
