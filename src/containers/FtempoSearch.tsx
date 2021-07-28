8f23d28190c0ea53ec7ffb81e3b0b524ee2bc4d8
Alastair Porter <alastair@porter.net.nz>
2021-07-28 15:24:09 +0200

    Make FtempoSearch a function

--- src/containers/FtempoSearch.tsx
+++ src/containers/FtempoSearch.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { parseMei } from "../search/SearchQuery";
 import {
   Accordion,
@@ -7,46 +7,26 @@ import {
   AccordionItemHeading,
   AccordionItemPanel,
 } from "react-accessible-accordion";
+import { Button, Form } from "react-bootstrap-v5";
 
-type FtempoSearchProps = {
+interface FtempoSearchProps {
   vrvToolkit: any;
-};
+}
 
-type FtempoSearchState = {
-  selectedOption?: string;
-  meiVoiceQueryStrings: { [key: string]: { label: string; notes: string } };
-  searchResults?: object[];
-};
+const FtempoSearch = (props: FtempoSearchProps) => {
+  const mei = props.vrvToolkit.getMEI();
+  const meiDoc = new DOMParser().parseFromString(mei, "text/xml");
+  const meiVoiceQueryStrings = parseMei(meiDoc);
+  const [selectedOption, setSelectedOption] = useState<string | undefined>(
+    undefined
+  );
+  const [searchResults, setSearchResults] = useState<object[]>([]);
 
-export default class FtempoSearch extends Component<
-  FtempoSearchProps,
-  FtempoSearchState
-> {
-  constructor(props: FtempoSearchProps) {
-    super(props);
-    const mei = props.vrvToolkit.getMEI();
-    const meiDoc = new DOMParser().parseFromString(mei, "text/xml");
-    console.debug(meiDoc);
-    this.state = {
-      selectedOption: undefined,
-      meiVoiceQueryStrings: parseMei(meiDoc),
-      searchResults: undefined,
-    };
-  }
-
-  onValueChange = (event: any) => {
-    this.setState({
-      selectedOption: event.target.value,
-      searchResults: undefined,
-    });
-  };
-
-  doSearch = () => {
-    if (!this.state.selectedOption) {
+  const doSearch = () => {
+    if (!selectedOption) {
       return;
     }
-    const selectedVoice =
-      this.state.meiVoiceQueryStrings[this.state.selectedOption];
+    const selectedVoice = meiVoiceQueryStrings[selectedOption];
     if (selectedVoice && selectedVoice.notes) {
       const query = { codestring: selectedVoice.notes };
       fetch("https://uk-dev-ftempo.rism.digital/api/query", {
@@ -58,73 +38,74 @@ export default class FtempoSearch extends Component<
       })
         .then((response) => response.json())
         .then((data) => {
-          this.setState({ searchResults: data });
+          setSearchResults(data);
         });
     }
   };
 
-  render() {
-    return (
-      <div className="ftempoContainer">
-        <h3>Search using F-TEMPO (experimental)</h3>
-        {this.state.meiVoiceQueryStrings && (
-          <>
-            <p>
-              Search this score on F-TEMPO: select a single voice from this list
-              as query
-            </p>
-            <ul>
-              {Object.keys(this.state.meiVoiceQueryStrings).map((e) => {
-                return (
-                  <li key={e}>
-                    <label>
-                      <input
-                        type="radio"
-                        name="voice"
-                        value={e}
-                        id={"voice-" + e}
-                        checked={this.state.selectedOption === e}
-                        onChange={this.onValueChange}
-                      />
-                      {this.state.meiVoiceQueryStrings[e].label}
-                    </label>
-                  </li>
-                );
-              })}
-              {/*<li><label><input type="radio" name="voice" value="all" checked={this.state.selectedOption === "all"} onChange={this.onValueChange} />All voices</label></li>*/}
-            </ul>
-            <button
-              onClick={this.doSearch}
-              disabled={!this.state.selectedOption}
-            >
-              Search
-            </button>
-          </>
-        )}
-        {this.state.searchResults && (
-          <Accordion allowZeroExpanded>
-            {this.state.searchResults.map((i: any) => {
+  return (
+    <div className="ftempoContainer">
+      <h3>Search using F-TEMPO (experimental)</h3>
+      {meiVoiceQueryStrings && (
+        <>
+          <p>
+            Search this score on F-TEMPO: select a single voice from this list
+            as query
+          </p>
+          <ul>
+            {Object.keys(meiVoiceQueryStrings).map((e) => {
               return (
-                <AccordionItem key={i.id}>
-                  <AccordionItemHeading>
-                    <AccordionItemButton>{i.id}</AccordionItemButton>
-                  </AccordionItemHeading>
-                  <AccordionItemPanel>
-                    <img
-                      src={
-                        "https://uk-dev-ftempo.rism.digital/img/jpg/" +
-                        i.id +
-                        ".jpg"
-                      }
-                      alt=""
-                    />
-                  </AccordionItemPanel>
-                </AccordionItem>
+                <Form.Check
+                  key={e}
+                  type="radio"
+                  name="voice"
+                  value={e}
+                  id={"voice-" + e}
+                  checked={selectedOption === e}
+                  onChange={(e) => {
+                    setSelectedOption(e.target.value);
+                    setSearchResults([]);
+                  }}
+                  label={meiVoiceQueryStrings[e].label}
+                />
               );
             })}
-          </Accordion>
-        )}
-      </div>
-    );
-  }
-}
+            {/*<li><label><input type="radio" name="voice" value="all" checked={this.state.selectedOption === "all"} onChange={this.onValueChange} />All voices</label></li>*/}
+          </ul>
+          <Button
+            variant="success"
+            onClick={doSearch}
+            disabled={!selectedOption}
+          >
+            Search
+          </Button>
+        </>
+      )}
+      {searchResults.length > 0 && (
+        <Accordion allowZeroExpanded>
+          {searchResults.map((i: any) => {
+            return (
+              <AccordionItem key={i.id}>
+                <AccordionItemHeading>
+                  <AccordionItemButton>{i.id}</AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <img
+                    src={
+                      "https://uk-dev-ftempo.rism.digital/img/jpg/" +
+                      i.id +
+                      ".jpg"
+                    }
+                    alt=""
+                  />
+                </AccordionItemPanel>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      )}
+    </div>
+  );
+};
+
+export default FtempoSearch;
