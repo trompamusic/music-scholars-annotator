02911389715a41d8de2d1da2249da8b49e0c6167
Alastair Porter <alastair@porter.net.nz>
2026-05-11 16:43:34 +0200

    Updates

---
+++ precache-manifest.a3261251153458a96b6fed4394797782.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d42f9074872be3962fa57f462ae8e4",
+    "url": "/music-scholars-annotator/index.html"
+  },
+  {
+    "revision": "c37a2fc2cdd241c571e6",
+    "url": "/music-scholars-annotator/static/css/2.222ec614.chunk.css"
+  },
+  {
+    "revision": "c37a2fc2cdd241c571e6",
+    "url": "/music-scholars-annotator/static/js/2.60b02434.chunk.js"
+  },
+  {
+    "revision": "1d18a1d0c266346e0823b198fc6144fa",
+    "url": "/music-scholars-annotator/static/js/2.60b02434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1c9c70ab116285c5de",
+    "url": "/music-scholars-annotator/static/js/main.7ba19ba2.chunk.js"
+  },
+  {
+    "revision": "7e4c496a1e429c9fca4b",
+    "url": "/music-scholars-annotator/static/js/runtime-main.f3023b88.js"
+  },
+  {
+    "revision": "ee4119b14a2697a436cc22c1457d4061",
+    "url": "/music-scholars-annotator/static/media/arrow-alt-to-top-regular.ee4119b1.svg"
+  },
+  {
+    "revision": "1bf90cefbcdde0ad30050cd8df2ee044",
+    "url": "/music-scholars-annotator/static/media/arrow-to-left-regular.1bf90cef.svg"
+  },
+  {
+    "revision": "f0cdfa6afaaa90446c6bd29f65781602",
+    "url": "/music-scholars-annotator/static/media/arrow-to-right-regular.f0cdfa6a.svg"
+  },
+  {
+    "revision": "b820b76daa4f057a1cbe26d6a34d6c58",
+    "url": "/music-scholars-annotator/static/media/file-import-regular.b820b76d.svg"
+  },
+  {
+    "revision": "41efc81e1c1d47a3b7d0b382ef7912b4",
+    "url": "/music-scholars-annotator/static/media/info-circle-regular.41efc81e.svg"
+  },
+  {
+    "revision": "f79fe6ae99a50939f12f755851398cf9",
+    "url": "/music-scholars-annotator/static/media/play-solid.f79fe6ae.svg"
+  },
+  {
+    "revision": "bc6bab640f884846a705c7a9f8b29396",
+    "url": "/music-scholars-annotator/static/media/search-minus-solid.bc6bab64.svg"
+  },
+  {
+    "revision": "5485d017b5ae571a128fb3a8d5bd70d1",
+    "url": "/music-scholars-annotator/static/media/search-plus-solid.5485d017.svg"
+  },
+  {
+    "revision": "a3e4c2f7c32ce9a7ad6fa8d381b9d9cf",
+    "url": "/music-scholars-annotator/static/media/trash-solid.a3e4c2f7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7d9e16fac0906639dd111aef7dd08a28.js"
+  "/music-scholars-annotator/precache-manifest.a3261251153458a96b6fed4394797782.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music-scholars-annotator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
